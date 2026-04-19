648e49c188ac7aa7ea0f591d16328a7de6f62555
Graboviyivan <ivangrabovij77@gmail.com>
2026-04-19 15:29:02 +0300

    update db

---
+++ js/db/game.js
@@ -0,0 +1,22 @@
+export const gameDb = [
+    {
+        title: 'Grand Theft Auto: San Andreas',
+        info: `Grand Theft Auto: San Andreas ( GTA San Andreas ) - комп'ютерна гра із серії Grand Theft Auto в жанрі 3D-шутера з елементами аркадного автосимулятора, розроблена Rockstar North . Це третя 3D-гра в серії та п'ята у лінійці основних проектів. Для консолі PlayStation 2 Grand Theft Auto: San Andreas була випущена 26 жовтня 2004 в Північній Америці і 29 жовтня 2004 в Європі та Австралії. На ПК та Xbox гра з'явилася 7 червня 2005 року у Північній Америці та 10 червня 2005 року у Європі. На ПК в Росії гра була офіційно локалізована та видана компанією 1C 12 лютого 2010 року.`,
+        img: `https://static.wikia.nocookie.net/gta/images/9/92/GTA_San_Andreas_Box_Art.jpg/revision/latest/scale-to-width-down/1200?cb=20220510003154&path-prefix=ru`,
+        walkthrough: `https://youtu.be/ZaqpcybxUqs?si=BKrAJpjC9uUHkDAV`,
+    },
+        {
+        title: 'Bully',
+        info: ``,
+        img: ``,
+        walkthrough: `https://youtu.be/nAIHZ5gqnps?si=OneHfVXuQzQ89lJF`,
+    },
+        {
+        title: 'Grand Theft Auto: San Andreas',
+        info: `Grand Theft Auto: San Andreas ( GTA San Andreas ) - комп'ютерна гра із серії Grand Theft Auto в жанрі 3D-шутера з елементами аркадного автосимулятора, розроблена Rockstar North . Це третя 3D-гра в серії та п'ята у лінійці основних проектів. Для консолі PlayStation 2 Grand Theft Auto: San Andreas була випущена 26 жовтня 2004 в Північній Америці і 29 жовтня 2004 в Європі та Австралії. На ПК та Xbox гра з'явилася 7 червня 2005 року у Північній Америці та 10 червня 2005 року у Європі. На ПК в Росії гра була офіційно локалізована та видана компанією 1C 12 лютого 2010 року.`,
+        img: `https://static.wikia.nocookie.net/gta/images/9/92/GTA_San_Andreas_Box_Art.jpg/revision/latest/scale-to-width-down/1200?cb=20220510003154&path-prefix=ru`,
+        walkthrough: `https://youtu.be/ZaqpcybxUqs?si=BKrAJpjC9uUHkDAV`,
+    },
+]
+
+console.log(gameDb[0].title)
