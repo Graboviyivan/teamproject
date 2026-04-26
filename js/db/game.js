5ef3c77b29ba179ab86ed5e7dc63dd46f6df3ae7
PixelShadow <akovlevm828@gmail.com>
2026-04-26 14:52:49 +0300

    add data base

--- js/db/game.js
+++ js/db/game.js
@@ -1,21 +1,27 @@
 export const gameDb = [
     {
         title: 'Grand Theft Auto: San Andreas',
-        info: `Grand Theft Auto: San Andreas ( GTA San Andreas ) - комп'ютерна гра із серії Grand Theft Auto в жанрі 3D-шутера з елементами аркадного автосимулятора, розроблена Rockstar North . Це третя 3D-гра в серії та п'ята у лінійці основних проектів. Для консолі PlayStation 2 Grand Theft Auto: San Andreas була випущена 26 жовтня 2004 в Північній Америці і 29 жовтня 2004 в Європі та Австралії. На ПК та Xbox гра з'явилася 7 червня 2005 року у Північній Америці та 10 червня 2005 року у Європі. На ПК в Росії гра була офіційно локалізована та видана компанією 1C 12 лютого 2010 року.`,
+        info: `Grand Theft Auto: San Andreas - комп'ютерна гра із серії Grand Theft Auto в жанрі 3D-шутера з елементами аркадного автосимулятора, розроблена Rockstar North . Це третя 3D-гра в серії та п'ята у лінійці основних проектів. Для консолі PlayStation 2 Grand Theft Auto: San Andreas була випущена 26 жовтня 2004 в Північній Америці і 29 жовтня 2004 в Європі та Австралії. На ПК та Xbox гра з'явилася 7 червня 2005 року у Північній Америці та 10 червня 2005 року у Європі. На ПК в Росії гра була офіційно локалізована та видана компанією 1C 12 лютого 2010 року.`,
         img: `https://static.wikia.nocookie.net/gta/images/9/92/GTA_San_Andreas_Box_Art.jpg/revision/latest/scale-to-width-down/1200?cb=20220510003154&path-prefix=ru`,
         walkthrough: `https://youtu.be/ZaqpcybxUqs?si=BKrAJpjC9uUHkDAV`,
     },
         {
         title: 'Bully',
-        info: ``,
-        img: ``,
+        info: `Bully - комп'ютерна гра в жанрі пригодницького бойовика звиглядом від третьої особи і відкритим світом , розроблена Rockstar Vancouver і випущена Rockstar Games в жовтні 2006`,
+        img: `https://m.media-amazon.com/images/M/MV5BZDk4OWM1NGEtNTU3Ny00NmNhLTgzM2MtM2M0MmU5NTQ4ODg0XkEyXkFqcGc@._V1_.jpg`,
         walkthrough: `https://youtu.be/nAIHZ5gqnps?si=OneHfVXuQzQ89lJF`,
     },
         {
-        title: 'Grand Theft Auto: San Andreas',
-        info: `Grand Theft Auto: San Andreas ( GTA San Andreas ) - комп'ютерна гра із серії Grand Theft Auto в жанрі 3D-шутера з елементами аркадного автосимулятора, розроблена Rockstar North . Це третя 3D-гра в серії та п'ята у лінійці основних проектів. Для консолі PlayStation 2 Grand Theft Auto: San Andreas була випущена 26 жовтня 2004 в Північній Америці і 29 жовтня 2004 в Європі та Австралії. На ПК та Xbox гра з'явилася 7 червня 2005 року у Північній Америці та 10 червня 2005 року у Європі. На ПК в Росії гра була офіційно локалізована та видана компанією 1C 12 лютого 2010 року.`,
-        img: `https://static.wikia.nocookie.net/gta/images/9/92/GTA_San_Andreas_Box_Art.jpg/revision/latest/scale-to-width-down/1200?cb=20220510003154&path-prefix=ru`,
-        walkthrough: `https://youtu.be/ZaqpcybxUqs?si=BKrAJpjC9uUHkDAV`,
+        title: 'Grand Theft Auto V',
+        info: `Grand Theft Auto: V - комп'ютерна гра в жанрі action-adventure з відкритим світом , розроблена компанією Rockstar North і видана компанією Rockstar Games . Спочатку гра була випущена для ігрових консолей PlayStation 3 і Xbox 360 у 2013 році , у 2014 році перевидана для PlayStation 4 та Xbox One , у 2015 році для Windows та у 2022 році для PlayStation 5 та Xbox Series X/S`,
+        img: `https://upload.wikimedia.org/wikipedia/ru/thumb/c/c8/GTAV_Official_Cover_Art.jpg/330px-GTAV_Official_Cover_Art.jpg`,
+        walkthrough: `https://youtu.be/7ssp44DydyA?si=VR32Ocs2T9DoZZvp`,
+    },
+        {
+        title: 'Grand Theft Auto IV',
+        info: `Grand Theft Auto IV - `,
+        img: `https://upload.wikimedia.org/wikipedia/ru/d/dd/Grand_Theft_Auto_IV.jpg`,
+        walkthrough: `https://youtu.be/O3QBb6GeDsY?si=binezV3tuK-ZIDKQ`,
     },
 ]
 
